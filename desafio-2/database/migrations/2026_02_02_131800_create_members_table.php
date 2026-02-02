78d527905ee1e2ee1171b95a968b80542fd4f555
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-02-02 10:29:05 -0300

    feat: adiciona modelo e migração para associados (members)

---
+++ desafio-2/database/migrations/2026_02_02_131800_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Address;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('state');
+            $table->string('city');
+        });
+
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('cpf')->index();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->foreignIdFor(Address::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
