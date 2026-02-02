6d8d2bc4928880a13ae852ae9821163de8e77097
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-02-02 14:57:31 -0300

    update(actions): permite atualização total ou parcial do associado

--- desafio-2/app/Actions/UpdateMemberAction.php
+++ desafio-2/app/Actions/UpdateMemberAction.php
@@ -2,29 +2,33 @@
 
 namespace App\Actions;
 
-use App\DTOs\MemberRegistrationDTO;
+use App\DTOs\MemberUpdateDTO;
 use App\Models\Address;
 use App\Models\Member;
 
 class UpdateMemberAction
 {
-    public function execute(Member $member, MemberRegistrationDTO $dto): Member
+    public function execute(Member $member, MemberUpdateDTO $dto): Member
     {
-        // Busca pelo endereço, caso não exista cria um novo endereço
-        $address = Address::firstOrCreate([
-            'city' => $dto->city,
-            'state' => $dto->state,
-        ]);
+        if ($dto->city && $dto->state) {
+            // Busca pelo endereço, caso não exista cria um novo endereço
+            $address = Address::firstOrCreate([
+                'city' => $dto->city,
+                'state' => $dto->state,
+            ]);
+            $member->address_id = $address->id;
+        }
 
-        $member->update(
+        $member->fill(array_filter(
             [
                 'name' => $dto->name,
                 'cpf' => $dto->cpf,
                 'email' => $dto->email,
                 'phone' => $dto->phone,
-                'address_id' => $address->id,
             ]
-        );
+        ));
+
+        $member->save();
 
         return $member->refresh();
     }
