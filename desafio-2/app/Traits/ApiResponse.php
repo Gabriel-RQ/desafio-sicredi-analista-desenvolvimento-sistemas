437fa076bee0208bbd4cfa641a1855df5ac06c40
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-02-02 17:09:52 -0300

    feat: adiciona trait para respostas da API padronizadas

---
+++ desafio-2/app/Traits/ApiResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Trait para padronização das respostas da API.
+ */
+trait ApiResponse
+{
+    /**
+     * Resposta de Sucesso
+     */
+    protected function success($data = null, ?string $message = null, int $code = 200): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    /**
+     * Resposta de Erro
+     */
+    protected function error(string $message, int $code = 400, $errors = null): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors,
+        ], $code);
+    }
+}
