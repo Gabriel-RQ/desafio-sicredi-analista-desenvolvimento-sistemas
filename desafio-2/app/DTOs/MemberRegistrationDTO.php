3ae0a81673923f5214093ffbf345754ab36f3338
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-02-02 13:42:51 -0300

    feat: adiciona MemberRegistrationDTO

---
+++ desafio-2/app/DTOs/MemberRegistrationDTO.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DTOs;
+
+use Illuminate\Http\Request;
+
+readonly class MemberRegistrationDTO
+{
+    public function __construct(
+        public string $cpf,
+        public string $name,
+        public string $phone,
+        public string $email,
+        public string $state,
+        public string $city,
+    ) {}
+
+    /**
+     * Obtém nova instância a partir de uma requisição.
+     */
+    public static function fromRequest(Request $request): MemberRegistrationDTO
+    {
+        return new self(
+            $request->cpf,
+            $request->name,
+            $request->phone,
+            $request->email,
+            ucfirst(strtolower(trim($request->state))),
+            ucfirst(strtolower(trim($request->city))),
+        );
+    }
+}
