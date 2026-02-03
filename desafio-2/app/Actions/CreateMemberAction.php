3837d8b74125f7987f08331a26312f595bfad568
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2026-02-03 08:45:22 -0300

    update(actions): usa exceções customizadas

--- desafio-2/app/Actions/CreateMemberAction.php
+++ desafio-2/app/Actions/CreateMemberAction.php
@@ -3,28 +3,36 @@
 namespace App\Actions;
 
 use App\DTOs\MemberRegistrationDTO;
+use App\Exceptions\MemberCreationException;
 use App\Models\Address;
 use App\Models\Member;
 use DB;
+use Log;
 
 class CreateMemberAction
 {
     public function execute(MemberRegistrationDTO $dto): Member
     {
         return DB::transaction(function () use ($dto) {
-            // Busca pelo endereço, caso não exista cria um novo endereço
-            $address = Address::firstOrCreate([
-                'city' => $dto->city,
-                'state' => $dto->state,
-            ]);
+            try {
+                // Busca pelo endereço, caso não exista cria um novo endereço
+                $address = Address::firstOrCreate([
+                    'city' => $dto->city,
+                    'state' => $dto->state,
+                ]);
 
-            return Member::create([
-                'name' => $dto->name,
-                'cpf' => $dto->cpf,
-                'email' => $dto->email,
-                'phone' => $dto->phone,
-                'address_id' => $address->id,
-            ]);
+                return Member::create([
+                    'name' => $dto->name,
+                    'cpf' => $dto->cpf,
+                    'email' => $dto->email,
+                    'phone' => $dto->phone,
+                    'address_id' => $address->id,
+                ]);
+            } catch (\Exception $e) {
+                Log::error('Falha ao criar associado: {msg}', ['msg' => $e->getMessage()]);
+
+                throw new MemberCreationException;
+            }
         });
     }
 }
--- desafio-2/app/Actions/UpdateMemberAction.php
+++ desafio-2/app/Actions/UpdateMemberAction.php
@@ -3,33 +3,41 @@
 namespace App\Actions;
 
 use App\DTOs\MemberUpdateDTO;
+use App\Exceptions\MemberUpdateException;
 use App\Models\Address;
 use App\Models\Member;
+use Log;
 
 class UpdateMemberAction
 {
     public function execute(Member $member, MemberUpdateDTO $dto): Member
     {
-        if ($dto->city && $dto->state) {
-            // Busca pelo endereço, caso não exista cria um novo endereço
-            $address = Address::firstOrCreate([
-                'city' => $dto->city,
-                'state' => $dto->state,
-            ]);
-            $member->address_id = $address->id;
+        try {
+            if ($dto->city && $dto->state) {
+                // Busca pelo endereço, caso não exista cria um novo endereço
+                $address = Address::firstOrCreate([
+                    'city' => $dto->city,
+                    'state' => $dto->state,
+                ]);
+                $member->address_id = $address->id;
+            }
+
+            $member->fill(array_filter(
+                [
+                    'name' => $dto->name,
+                    'cpf' => $dto->cpf,
+                    'email' => $dto->email,
+                    'phone' => $dto->phone,
+                ]
+            ));
+
+            $member->save();
+
+            return $member->refresh();
+        } catch (\Exception $e) {
+            Log::error('Falha ao atualizar dados do associado: {msg}', ['msg' => $e->getMessage()]);
+
+            throw new MemberUpdateException;
         }
-
-        $member->fill(array_filter(
-            [
-                'name' => $dto->name,
-                'cpf' => $dto->cpf,
-                'email' => $dto->email,
-                'phone' => $dto->phone,
-            ]
-        ));
-
-        $member->save();
-
-        return $member->refresh();
     }
 }
